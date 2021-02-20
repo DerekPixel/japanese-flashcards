a939b8670eccba024ffa37494d779bc0393ca1ec
Derek Price <derekp098@gmail.com>
2021-02-19 23:02:04 -0800

    added support to read from card array and display data

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react'
 
 const Flashcard = ({cards}) => {
-
-  const [randoNum, setrandoNum] = useState(Math.floor(Math.random() * cards.length));
-
-  var maxNum = cards.length;
-
-  var newRandomNum = (maxNum) => {
-    return Math.floor(Math.random() * maxNum);
-  }
 
   return (
     <div>
       <div className="flip-card">
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <h1>{cards[randoNum].japanese}</h1>
-            <p>{randoNum}</p>
-      
+            <h2>{cards[0].japanese}</h2>
           </div>
           <div className="flip-card-back">
-            <h1>{cards[randoNum].eng}</h1>
-            <p>{randoNum}</p>
+            <h2>{cards[0].eng}</h2>
           </div>
         </div>
       </div>
-      <button onClick={() => setrandoNum(newRandomNum(maxNum))} >New Card</button>
     </div>
   )
 }
