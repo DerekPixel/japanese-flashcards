4df18a1547238dd98a3f317b363287ff6fff8bb2
Derek Price <derekp098@gmail.com>
2021-07-18 19:53:22 -0700

    reworked newcard component to accommodate the categories

--- src/components/Newcard.jsx
+++ src/components/Newcard.jsx
@@ -1,13 +1,57 @@
+import React, { useState } from "react";
+import { duplicateObjectsInArrayOrObject, shuffleArray } from '../functions.jsx';
 
-const Newcard = ({engInput, japaneseInput, engChange, japChange, onClick}) => {
+const Newcard = ({allcards, setAllCards, setRandomizedCards, resetCard}) => {
+
+  const [japaneseInput, setJapaneseInput] = useState('');
+  const [engInput, setEngInput] = useState('');
+  const [categoryInput, setCategpryInput] = useState('');
+
+  var options = Object.keys(allcards).map((keyname, i, list) => {
+    return (
+      <option 
+        key={keyname}
+        value={keyname}
+      >{keyname}</option>
+    )
+  })
+
+  function pushNewFlashcardToCardsArrayAndUpdateLocalStorage() {
+    var newFlashcard = {};
+
+    newFlashcard.japanese = japaneseInput;
+    newFlashcard.eng = engInput;
+
+    setJapaneseInput('');
+    setEngInput('');
+
+    var allCardsCopy = duplicateObjectsInArrayOrObject(allcards);
+    allCardsCopy[categoryInput].cards.push(newFlashcard);
+
+    resetCard();
+
+    setAllCards(allCardsCopy);
+    setRandomizedCards(shuffleArray(allCardsCopy[categoryInput].cards));
+
+    window.localStorage.setItem('usersFlashcards', JSON.stringify(allCardsCopy));
+  }
+
   return (
 
     <div className='form'>
       <label htmlFor='japanese' >Japanese</label>
-      <textarea name="japanese" id="jap" value={japaneseInput} onChange={japChange} ></textarea>
+      <textarea name="japanese" id="jap" value={japaneseInput} onChange={(e) => setJapaneseInput(e.target.value)} ></textarea>
       <label htmlFor="english">English</label>
-      <textarea name="english" id="eng" value={engInput} onChange={engChange} ></textarea>
-      <button onClick={onClick} >Make New Card</button>
+      <textarea name="english" id="eng" value={engInput} onChange={(e) => setEngInput(e.target.value)} ></textarea>
+      <label htmlFor="category">Choose a Category</label>
+      <select 
+        name="category" 
+        defaultValue={allcards[Object.keys(allcards)[0]]}
+        onChange={(e) => setCategpryInput(e.target.value)}
+      >
+        {options}
+      </select>
+      <button onClick={() => pushNewFlashcardToCardsArrayAndUpdateLocalStorage()} >Make New Card</button>
     </div>
 
   )
