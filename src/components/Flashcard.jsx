7e4fad1c27cbdfcca950f291e2be85a46aa9f0f2
Derek <derekp098@gmail.com>
2022-02-08 01:29:05 -0800

    added a class

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -12,11 +12,11 @@ const Flashcard = ({cards, onDelete, flip, visiable,  onClick}) => {
       <div onClick={onClick} className="flip-card" >
         <div className="flip-card-inner" id={flip}>
           <div className="flip-card-front">
-            <p>{jap}</p>
+            <p className="flip-card-text" >{jap}</p>
           </div>
           <div className="flip-card-back">
             {
-              visiable && <p>{eng}</p>
+              visiable && <p className="flip-card-text" >{eng}</p>
             }
           </div>
         </div>
